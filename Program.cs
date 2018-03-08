d7c1b48219513cb2ba0b3264ac035ec0f7d3969b
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2018-03-08 16:36:55 +1300

    done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = 74;
+            var num2 = 36;
+
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            //done//
+
+        }
+    }
+}
